2cd6b5679ec728cf4844323c09563ea1bbb7782d
Esther van Driessche <esther.van.driessche@student.howest.be>
2025-03-18 09:36:27 +0100

    removed the automatic base

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import initialRestaurant from "./restaurant";
+import './App.css';
+import { BrowserRouter, Routes, Route, NavLink } from "react-router";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [restaurant, setRestaurant] = useState(initialRestaurant);
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="container cols">
+        <nav>
+          <ul>
+            {Object.values(restaurant.tables).map((table) => (
+            <li>
+                <NavLink to="/">{table.name}</NavLink>
+            </li>
+            ))}
+        
+          </ul>
+        </nav>
+        <div>
+          <Routes>
+            <Route path="/" />
+            <Route path="/edit" />
+          </Routes>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/restaurant.js
@@ -0,0 +1,28 @@
+const restaurant = {
+    tables: {
+        1: {
+            id: 1,
+            name: 'table 1',  
+            seated: true,   
+        },
+        2: {
+            id: 2,
+            name: 'table 2',
+            seated: false
+        }
+    },
+    menu: {
+        1:{
+            id: 1, 
+            name: 'spaghetti',
+            price: 22.99,
+        },
+        2: {
+            id: 2,
+            name: 'tiramisu',
+            price: 15.55,
+        },
+    }
+}
+
+export default restaurant;
