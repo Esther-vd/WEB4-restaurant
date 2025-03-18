97220f056c3cc018f49b9af4d79fcd130d42208f
Esther van Driessche <esther.van.driessche@student.howest.be>
2025-03-18 11:32:40 +0100

    fixed vite.config.js base name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Web4-restaurant/'
+  base: '/WEB4-restaurant/'
 })
